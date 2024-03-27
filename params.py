edbe3d8b305a4866285a778a57d814cc323793e4
Kenny Tan <kenny.tan@sti.valantic.com>
2024-03-27 15:20:32 +0100

    cloned from source https://github.com/mongodb-developer/atlas-langchain

---
+++ params.py
@@ -0,0 +1,6 @@
+openai_api_key = '<OpenAI API Key>'
+
+mongodb_conn_string = '<Application connection string of Atlas cluster>'
+db_name = "search_db"
+collection_name = "search_col"
+index_name = "vsearch_index"
---
+++ vectorize.py
@@ -0,0 +1,42 @@
+# https://python.langchain.com/docs/modules/data_connection/vectorstores/integrations/mongodb_atlas
+
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.document_loaders import WebBaseLoader
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import MongoDBAtlasVectorSearch
+from pymongo import MongoClient
+import params
+
+# Step 1: Load
+loaders = [
+ WebBaseLoader("https://en.wikipedia.org/wiki/AT%26T"),
+ WebBaseLoader("https://en.wikipedia.org/wiki/Bank_of_America")
+]
+data = []
+for loader in loaders:
+    data.extend(loader.load())
+
+# Step 2: Transform (Split)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separators=[
+                                               "\n\n", "\n", "(?<=\. )", " "], length_function=len)
+docs = text_splitter.split_documents(data)
+print('Split into ' + str(len(docs)) + ' docs')
+
+# Step 3: Embed
+# https://api.python.langchain.com/en/latest/embeddings/langchain.embeddings.openai.OpenAIEmbeddings.html
+embeddings = OpenAIEmbeddings(openai_api_key=params.openai_api_key)
+
+# Step 4: Store
+# Initialize MongoDB python client
+client = MongoClient(params.mongodb_conn_string)
+collection = client[params.db_name][params.collection_name]
+
+# Reset w/out deleting the Search Index 
+collection.delete_many({})
+
+# Insert the documents in MongoDB Atlas with their embedding
+# https://github.com/hwchase17/langchain/blob/master/langchain/vectorstores/mongodb_atlas.py
+docsearch = MongoDBAtlasVectorSearch.from_documents(
+    docs, embeddings, collection=collection, index_name=params.index_name
+)
